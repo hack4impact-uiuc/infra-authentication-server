bc37dc31844883ae6a7181dcda9db0d651c3ee73
Josh Byster <joshb1050@gmail.com>
2019-02-13 02:09:51 -0600

    Fixed deprecated API call to mongoose

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const uri = "mongodb://product:infra28@ds125385.mlab.com:25385/auth-infra"; 
-const db = mongoose.createConnection(uri);
+const db = mongoose.connect(uri,{useNewUrlParser: true});
 
-const schema = mongoose.Schema({ email: "string", username: "string" , password: "string"});
-const User = db.model("User", schema);
+const schema = mongoose.Schema({ email: "string", username: "string" , password: "string"},);
+const User = mongoose.model("User", schema);
 
 module.exports = User;
