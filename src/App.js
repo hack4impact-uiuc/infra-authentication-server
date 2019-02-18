5619d275280d4802713bb59e123657fde849699b
Helena Chi <hchi124@gmail.com>
2019-02-18 16:09:29 -0600

    i hope im doing something right

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const User = require("./models/User");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
   res.send("Hello World");
@@ -29,8 +32,22 @@ app.get("/put/:name", function(req, res) {
   res.send("Added User " + req.params.name);
 });
 
-app.get("signup", async function(req, res) {
+// app.get("/signup",  function(req, res) {
+//   res.send("this is /signup, where you can create an account");
+// });
 
+app.post("/signup", async function(req, res, next) {
+  if (!req.body) return res.sendStatus(400);
+  const user = new User(req.body);
+  user.save()
+    .then(user => {
+       console.log('User added successfully');
+    })
+    // .catch(err => {
+    //   res.status(400).send("Unable to save to database. Please check that the input fields are filled in correctly.");
+    // });
+  res.send("email: " + req.body.email + "\nusername: " + req.body.username);
+  // res.redirect("/login") // supposedly the login page to enter account credentials to get to /login/:username/:password
 });
 
 app.listen(8000, function() {
