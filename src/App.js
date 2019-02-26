935143734aa1d5fa9b0d21d0251163f2d5eb845c
Josh Byster <8664074+josh-byster@users.noreply.github.com>
2019-02-25 20:17:46 -0600

    Forgot Password (#5)
    * Got basic sendgrid integration
    
    * Fixed deprecated API call to mongoose
    
    * added forgot route
    
    * Added support for email sending
    
    * Added start of docs
    
    * updated
    
    * Updated specs
    
    * Added all documentation for the forgot password endpoints
    
    * Fixed remaining merge conflict
    
    * Moved forgot password doc into correct directory
    
    * Removed sendgrid integration
    
    * Got /forgot_password/ endpoint working
    
    * Cleaned up details on /forgotPassword
    
    * Built out password reset endpoint
    
    * Finished securityQuestion endpoint
    
    * minor fixups
    
    * Formatting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-
 const User = require("./models/User");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,8 +16,8 @@ app.get("/", function(req, res) {
 });
 
 app.post("/auth", async function(req, res) {
-  console.log("auth")
-  res.send({"result":"success","token": "magic"})
+  console.log("auth");
+  res.send({ result: "success", token: "magic" });
 });
 
 app.get("/users", async function(req, res) {
@@ -26,24 +27,147 @@ app.get("/users", async function(req, res) {
 });
 
 app.get("/put/:name", function(req, res) {
-  var user = new User({ username: req.params.name, passord:"demo" });
+  var user = new User({ username: req.params.name, passord: "demo" });
   user.save();
   console.log("Added User " + req.params.name);
   res.send("Added User " + req.params.name);
 });
 
-app.get("/register",  function(req, res) {
-  res.send("this is /register, where you can put your information in a form to create an account");
+app.get("/register", function(req, res) {
+  res.send(
+    "this is /register, where you can put your information in a form to create an account"
+  );
 });
 
 app.post("/register", async function(req, res, next) {
   if (!req.body) return res.sendStatus(400);
   const user = new User(req.body);
-  await user.save()
-    .then(user => {
-       console.log("User added successfully");
-    });
+  await user.save().then(user => {
+    console.log("User added successfully");
+  });
   res.send("email: " + req.body.email + "\nusername: " + req.body.username);
+});
+
+app.post("/forgotPassword", async function(req, res) {
+  const user = await User.findOne({ email: req.body.email }).catch(e =>
+    console.log(e)
+  );
+  console.log(user.expiration);
+  // TODO: handle the config file change in security question
+  if (
+    user &&
+    req.body.answer &&
+    user.answer === req.body.answer.toLowerCase()
+  ) {
+    //user found, update pin
+    user.pin = Math.floor(Math.random() * (100000000 - 100000 + 1)) + 100000;
+    var date = new Date();
+    // add a day to the current date
+    date.setDate(date.getDate() + 1);
+    user.expiration = date;
+    await user.save();
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.INFRA_EMAIL,
+        clientId: process.env.INFRA_CLIENT_ID,
+        clientSecret: process.env.INFRA_CLIENT_SECRET,
+        refreshToken: process.env.INFRA_REFRESH_TOKEN
+      }
+    });
+    console.log("Created transport with nodemailer");
+    transporter
+      .sendMail({
+        from: "hack4impact.infra@gmail.com",
+        to: user.email,
+        subject: "Forgot Password",
+        text: "Enter the following pin on the reset page: " + user.pin
+      })
+      .catch(e => {
+        console.log(e);
+        res.send({
+          status: 500,
+          message:
+            "An internal server error occured and the email could not be sent."
+        });
+      });
+    res.send({
+      status: 200,
+      message: "Sent password reset PIN to user if they exist in the database."
+    });
+  } else {
+    res.send({
+      status: 400,
+      message: "User does not exist in the DB."
+    });
+  }
+});
+
+app.post("/passwordReset", async function(req, res) {
+  const user = await User.findOne({ email: req.body.email }).catch(e =>
+    console.log(e)
+  );
+  if (!user) {
+    res.send({
+      status: 400,
+      message: "User does not exist in the database"
+    });
+    return;
+  }
+  if (user.pin != req.body.pin) {
+    res.send({
+      status: 400,
+      message: "PIN does not match"
+    });
+    return;
+  }
+  if (user.expiration.getTime() < new Date().getTime()) {
+    res.send({
+      status: 400,
+      message: "PIN is expired"
+    });
+    return;
+  }
+  //user matches, change expiration
+  var date = new Date();
+  // remove a day to the current date to expire it
+  // set date to 24 hours before because we don't want
+  // concurrent requests happening in the same second to both go through
+  // (i.e. if the user presses change password button twice)
+  date.setDate(date.getDate() - 1);
+  user.expiration = date;
+  user.password = req.body.password;
+  await user.save();
+
+  res.send({
+    status: 200,
+    message: "Password successfully reset"
+  });
+});
+
+app.post("/getSecurityQuestion", async function(req, res) {
+  const user = await User.findOne({ email: req.body.email }).catch(e => {
+    console.log(e);
+  });
+  if (!user) {
+    res.send({
+      status: 400,
+      message: "User does not exist in the database"
+    });
+    return;
+  }
+  if (!user.question) {
+    res.send({
+      status: 400,
+      message: "No security question set"
+    });
+    return;
+  }
+  res.send({
+    status: 200,
+    question: user.question
+  });
 });
 
 app.listen(8000, function() {
--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
-const uri = "mongodb://product:infra28@ds125385.mlab.com:25385/auth-infra"; 
-const db = mongoose.createConnection(uri);
+const uri = "mongodb://product:infra28@ds125385.mlab.com:25385/auth-infra";
+const db = mongoose.connect(uri, { useNewUrlParser: true });
 
-const schema = mongoose.Schema({ email: "string", username: "string" , password: "string"});
-const User = db.model("User", schema);
+const schema = mongoose.Schema({
+  username: "string",
+  password: "string",
+  email: "string",
+  question: "string",
+  answer: "string",
+  pin: "number",
+  expiration: "date"
+});
+const User = mongoose.model("User", schema);
 
 module.exports = User;
