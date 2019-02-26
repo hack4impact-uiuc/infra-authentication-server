f7245d6641cef2ed5f5c7a0b6f2a7cdb2197e235
Helena Chi <hchi124@gmail.com>
2019-02-25 20:30:57 -0600

    Login/Register

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ const User = require("./models/User");
 const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const exjwt = require("express-jwt");
+
+// var SECRET_TOKEN = process.env.SECRET_TOKEN;
+var SECRET_TOKEN = "helga_has_n000000_idea_what_she_doin";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const router = require("./api/password-reset.js");
@@ -38,19 +43,90 @@ app.get("/put/:name", function(req, res) {
   res.send("Added User " + req.params.name);
 });
 
-app.get("/register", function(req, res) {
-  res.send(
-    "this is /register, where you can put your information in a form to create an account"
-  );
-});
-
 app.post("/register", async function(req, res, next) {
-  if (!req.body) return res.sendStatus(400);
-  const user = new User(req.body);
+  // no email or password provided --> invalid
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({
+      status: 400,
+      message: "Please enter valid arguments for the fields provided."
+    });
+  }
+
+  // email already in database --> invalid
+  if (await User.findOne({ email: req.body.email })) {
+    return res.status(400).send({
+      status: 400,
+      message: "User already exists. Please try again."
+    });
+  }
+
+  // create user with given form input data
+  /**
+   * {
+   *    //uuid: automatically done in when sent to db? i think
+   *    email: string,
+   *    token: string, generated in a more secure way than what i have rn lol
+   * }
+   */
+  // token generated with email and password with our "secret_token"
+  const jwt_token = jwt.sign(
+    { email: req.body.email, password: req.body.password },
+    SECRET_TOKEN
+  );
+  const user_data = {
+    email: req.body.email,
+    password: jwt_token
+  };
+  const user = new User(user_data);
   await user.save().then(user => {
     console.log("User added successfully");
   });
-  res.send("email: " + req.body.email + "\nusername: " + req.body.username);
+  return res.status(200).send({
+    status: 200,
+    message: "User added successfully!",
+    token: jwt_token
+  });
+});
+
+app.post("/login", async function(req, res) {
+  // no email or password provided --> invalid
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({
+      status: 400,
+      message: "Please enter valid arguments for the fields provided."
+    });
+  }
+  // un-jwt-ify the given password, see if it's a match with the token associated with the email.
+  var user = await User.findOne({ email: req.body.email });
+  if (user) {
+    var decoded = jwt.verify(user.password, SECRET_TOKEN, {
+      password: req.body.password
+    });
+    if (req.body.password === decoded.password) {
+      return res.status(200).send({
+        status: 200,
+        message: "Successful login!",
+        token: user.password
+      });
+    } else {
+      return res.status(400).send({
+        status: 400,
+        message: "Passwword incorrect. Please try again."
+      });
+    }
+  } else {
+    return res.status(400).send({
+      status: 400,
+      message:
+        "The information you provided does not match our database. Please check your inputs again."
+    });
+  }
+});
+
+app.post("/forgotPassword", async function(req, res) {
+  const user = await User.findOne({ email: req.body.email }).catch(e =>
+    console.log(e)
+  );
 });
 
 module.exports = { app };
