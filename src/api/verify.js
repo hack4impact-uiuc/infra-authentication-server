ac58a2911acd372a26e3ec40c6f2353f87ad1610
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2019-03-14 20:35:37 -0500

    secret

--- src/api/verify.js
+++ src/api/verify.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const User = require("../models/User");
 const fetch = require("node-fetch");
 const { sendResponse } = require("./../utils/sendResponse");
-require("dotenv").config();
+var SECRET_TOKEN = "helga_has_n000000_idea_what_she_doin";
 
 router.post("/verify", async function(req, res) {
   let authenticationStatus = {};
   try {
-    authenticationStatus = jwt.verify(req.headers.token, SECRET_TOKEN);
+    authenticationStatus = jwt.verify(req.body.token, SECRET_TOKEN);
   } catch (e) {
     return sendResponse(res, 400, "Invalid Token");
   }
