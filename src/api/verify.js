a5d8f8714cc137347080505aa4474874ab36d164
arshiam2 <arshiam2@illinois.edu>
2019-03-21 20:33:01 -0700

    moved all token parsing to the header

--- src/api/verify.js
+++ src/api/verify.js
@@ -6,15 +6,13 @@ var SECRET_TOKEN = "helga_has_n000000_idea_what_she_doin";
 const { decryptAuthJWT, verifyAuthJWT } = require("./../utils/jwtHelpers");
 
 router.post("/verify", async function(req, res) {
-  console.log(req.body.token);
-  var userId = decryptAuthJWT(req.body.token);
+  var userId = decryptAuthJWT(req.header.token);
   // Do a lookup by the decrypted user id
   const user = await User.findOne({ _id: userId }).catch(e => console.log(e));
   if (
     userId === null ||
-    !verifyAuthJWT(req.body.token, userId, user.password)
+    !verifyAuthJWT(req.header.token, userId, user.password)
   ) {
-    console.log(req.body.token);
     sendResponse(res, 400, "Invalid JWT token");
   } else if (user) {
     return res.status(200).send({
