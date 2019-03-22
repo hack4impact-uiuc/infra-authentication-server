73187c0dc7ad6de3127126439843d08ba91493d0
Josh Byster <joshb1050@gmail.com>
2019-03-22 00:05:39 -0500

    Added express validator

--- src/api/forgotPassword.js
+++ src/api/forgotPassword.js
@@ -1,47 +1,61 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator/check");
 const { sendResponse } = require("./../utils/sendResponse");
 const { isGmailEnabled } = require("../utils/getConfigFile");
 const { sendMail } = require("../utils/sendMail");
 const { generateAndCommitPIN } = require("../utils/pinHelpers");
 
-router.post("/forgotPassword", async function(req, res) {
-  const usingGmail = await isGmailEnabled();
-  if (!usingGmail) {
-    return sendResponse(
-      res,
-      500,
-      "Gmail not enabled. Do not use this endpoint."
+router.post(
+  "/forgotPassword",
+  [
+    check("email").isEmail(),
+    check("answer")
+      .isString()
+      .isLength({ min: 1 })
+  ],
+  async function(req, res) {
+    // Input validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return sendResponse(res, 400, "Invalid request", {
+        errors: errors.array({ onlyFirstError: true })
+      });
+    }
+
+    const usingGmail = await isGmailEnabled();
+    if (!usingGmail) {
+      return sendResponse(
+        res,
+        500,
+        "Gmail not enabled. Do not use this endpoint."
+      );
+    }
+    const user = await User.findOne({ email: req.body.email }).catch(e =>
+      console.log(e)
     );
-  }
-  if (!req.body || !req.body.email || !req.body.answer) {
-    return sendResponse(res, 400, "Malformed request");
-  }
 
-  const user = await User.findOne({ email: req.body.email }).catch(e =>
-    console.log(e)
-  );
-
-  // TODO: handle the config file change in security question
-  if (!user) {
-    return sendResponse(res, 400, "User does not exist in the DB.");
+    // TODO: handle the config file change in security question
+    if (!user) {
+      return sendResponse(res, 400, "User does not exist in the DB.");
+    }
+    if (
+      req.body.answer &&
+      user.answer === req.body.answer.toLowerCase().replace(/\s/g, "")
+    ) {
+      generateAndCommitPIN(user);
+      const body = {
+        from: "hack4impact.infra@gmail.com",
+        to: user.email,
+        subject: "Forgot Password",
+        text: "Enter the following pin on the reset page: " + user.pin
+      };
+      await sendMail(body);
+      sendResponse(res, 200, "Sent password reset PIN to user if they exist");
+    } else {
+      sendResponse(res, 400, "Answer to security question doesn't match");
+    }
   }
-  if (
-    req.body.answer &&
-    user.answer === req.body.answer.toLowerCase().replace(/\s/g, "")
-  ) {
-    generateAndCommitPIN(user);
-    const body = {
-      from: "hack4impact.infra@gmail.com",
-      to: user.email,
-      subject: "Forgot Password",
-      text: "Enter the following pin on the reset page: " + user.pin
-    };
-    await sendMail(body);
-    sendResponse(res, 200, "Sent password reset PIN to user if they exist");
-  } else {
-    sendResponse(res, 400, "Answer to security question doesn't match");
-  }
-});
+);
 
 module.exports = router;
--- src/api/google.js
+++ src/api/google.js
@@ -1,40 +1,54 @@
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator/check");
 const User = require("../models/User");
 const fetch = require("node-fetch");
 const { googleAuth } = require("./../utils/getConfigFile");
 const { sendResponse } = require("./../utils/sendResponse");
 require("dotenv").config();
 
-router.post("/google", async function(req, res) {
-  const useGoogle = await googleAuth();
-  if (!useGoogle)
-    return sendRespose(res, 400, "Google authentication has not be enabled");
-  if (!req.body) return sendRespose(res, 400, "Invalid request");
+router.post(
+  "/google",
+  check("tokenId")
+    .isString()
+    .isLength({ min: 1 }),
+  async function(req, res) {
+    // Input validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return sendResponse(res, 400, "Invalid request", {
+        errors: errors.array({ onlyFirstError: true })
+      });
+    }
 
-  const tokenInfoRes = await fetch(
-    `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${
-      req.body.tokenId
-    }`
-  );
-  const payload = await tokenInfoRes.json();
+    const useGoogle = await googleAuth();
+    if (!useGoogle)
+      return sendRespose(res, 400, "Google authentication has not be enabled");
 
-  const user = await User.findOne({ email: payload.email, googleAuth: true });
-  if (user) {
-    console.log("Welcome back " + user.username);
-    sendResponse(res, 200, "Successful login!");
-  } else {
-    const user = new User({
-      email: payload.email,
-      username: payload.name,
-      password: null,
-      googleAuth: true,
-      userLevel: "generalUser"
-    });
-    await user.save().then(user => {
-      console.log("Google user added successfully");
-    });
-    sendResponse(res, 200, "New Google user: " + payload.email);
+    const tokenInfoRes = await fetch(
+      `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${
+        req.body.tokenId
+      }`
+    );
+    const payload = await tokenInfoRes.json();
+
+    const user = await User.findOne({ email: payload.email, googleAuth: true });
+    if (user) {
+      console.log("Welcome back " + user.username);
+      sendResponse(res, 200, "Successful login!");
+    } else {
+      const user = new User({
+        email: payload.email,
+        username: payload.name,
+        password: null,
+        googleAuth: true,
+        userLevel: "generalUser"
+      });
+      await user.save().then(user => {
+        console.log("Google user added successfully");
+      });
+      sendResponse(res, 200, "New Google user: " + payload.email);
+    }
   }
-});
+);
 
 module.exports = router;
