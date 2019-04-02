5ce9f0f8e2675e777798bfb706859b8834aa927d
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2019-04-01 20:26:06 -0500

    setting security question requires password

--- src/api/addSecurityQuestion.js
+++ src/api/addSecurityQuestion.js
@@ -3,6 +3,9 @@ const User = require("../models/User");
 const { check, validationResult } = require("express-validator/check");
 const { sendResponse } = require("./../utils/sendResponse");
 const { decryptAuthJWT } = require("../utils/jwtHelpers");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET_TOKEN = "helga_has_n000000_idea_what_she_doin";
 
 router.post(
   "/addSecurityQuestion",
@@ -25,9 +28,28 @@ router.post(
         errors: errors.array({ onlyFirstError: true })
       });
     }
-    const userId = decryptAuthJWT(req.headers.token);
-    var user = await User.findOne({ _id: userId });
-    if (user) {
+    if (!req.headers.token) {
+      return sendResponse(res, 400, "Token not provided");
+    }
+    let authenticationStatus = {};
+    try {
+      authenticationStatus = jwt.verify(req.headers.token, SECRET_TOKEN);
+    } catch (e) {
+      return sendResponse(res, 400, "Invalid Token");
+    }
+    const user = await User.findById(authenticationStatus.userId);
+    if (!user) {
+      sendResponse(res, 400, "User does not exist in the database");
+      return;
+    } else {
+      let authenticated = false;
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        // hash matches! sign a JWT with an expiration 1 day in the future and send back to the user
+        authenticated = true;
+      }
+      if (!authenticated) {
+        return sendResponse(res, 400, "Incorrect Password");
+      }
       await User.updateOne(
         { _id: user._id },
         {
@@ -36,8 +58,6 @@ router.post(
         }
       );
       sendResponse(res, 200, "Succesfully added the security question");
-    } else {
-      sendResponse(res, 400, "User doesn't exist");
     }
   }
 );
